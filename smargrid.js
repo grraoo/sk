33596160343e1f85018617845dd7e2eb0bab87e8
Grraoo <evil@grraoo.ru>
2017-08-23 11:05:14 +0800

    initial

---
+++ smargrid.js
@@ -0,0 +1,28 @@
+var smartgrid = require('smart-grid');
+
+/* It's principal settings in smart grid project */
+var settings = {
+    outputStyle: 'scss', /* less || scss || sass || styl */
+    columns: 12, /* number of grid columns */
+    offset: "30px", /* gutter width px || % */
+    container: {
+        maxWidth: '1200px', /* max-width оn very large screen */
+        fields: '15px' /* side fields */
+    },
+    breakPoints: {
+        xs: {
+            'width': '375px', /* -> @media (max-width: 1100px) */
+            'fields': '15px' /* side fields */
+        }
+        /*
+        We can create any quantity of break points.
+
+        some_name: {
+            some_width: 'Npx',
+            some_offset: 'N(px|%)'
+        }
+        */
+    }
+};
+
+smartgrid('src/precss/lib', settings);
