c2d133a460c78c37cfe632108b9361123ef54341
Grraoo <evil@grraoo.ru>
2017-08-25 17:48:23 +0800

    autoplay

--- build/js/main.js
+++ build/js/main.js
@@ -1,31 +1,39 @@
-$(document).ready(function(){
-  $('.projects__wrap').slick({
-    arrows: true,
-    dots: false,
-    slidesToShow: 3,
-    slidesToScroll: 1
-  });
-  
-  $('.thanks__wrap').slick({
-    arrows: true,
-    dots: false,
-    slidesToShow: 3,
-    slidesToScroll: 1
-  });
-  
-  $('.trust__wrap').slick({
-    arrows: true,
-    dots: false,
-    slidesToShow: 4,
-    slidesToScroll: 1
-    // variableWidth: true,
-    // centerMode: true
-  });
+$(document).ready(function () {
+	$('.projects__wrap').slick({
+		arrows: true,
+		dots: false,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 4000,
+	});
+
+	$('.thanks__wrap').slick({
+		arrows: true,
+		dots: false,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 4000,
+	});
+
+	$('.trust__wrap').slick({
+		arrows: true,
+		dots: false,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		// variableWidth: true,
+		// centerMode: true
+	});
 });
 
-$('.scroll-btn--top').click(function(e){
-  e.preventDefault();
-  $('html, body').animate({scrollTop:$('body').position().top}, 2000);
+$('.scroll-btn--top').click(function (e) {
+	e.preventDefault();
+	$('html, body').animate({
+		scrollTop: $('body').position().top
+	}, 2000);
 });
 
 // Google Maps JS & Settings Coordinates
@@ -35,66 +43,66 @@ $('.scroll-btn--top').click(function(e){
 google.maps.event.addDomListener(window, 'load', init);
 
 function init() {
-// Basic options for a simple Google Map
-	
-	
+	// Basic options for a simple Google Map
+
+
 	//----------------- ADD YOUR SETTINGS HERE -----------------//
-	
-	
+
+
 	// Add your coordinates. How to know coordinates: https://support.google.com/maps/answer/18539?hl=en
 	var myLatlng = new google.maps.LatLng(52.268155, 104.342619);
-	
+
 	// Add your company name and some text about company
 	var maptooltipbold = 'Строй Комфорт';
 	var maptooltip = 'Архитектурно-строительная компания';
-	
-	
+
+
 	//---------------------------------------------------------//
-	
-	
+
+
 	var mapOptions = {
-		
+
 		// How zoomed in you want the map to start at (always required)
 		zoom: 18,
-		
+
 		// The latitude and longitude to center the map (always required)
 		center: myLatlng
 	};
 
 
-	
+
 	// Get the HTML DOM element that will contain your map 
 	// We are using a div with id="map" seen below in the <body>
 	var mapElement = document.getElementById('map');
 
 	// Create the Google Map using out element and options defined above
 	var map = new google.maps.Map(mapElement, mapOptions);
-	
+
 	// Image of toogle
 	var image = 'img/contacts-map-marker.png';
 
 	// Div's of toogle
 	var content = document.createElement('div');
-	content.innerHTML = "<div class="+"map-tooltip"+"><h4><strong>"+maptooltipbold+"</strong></h4><hr>"+"<h5>"+maptooltip+"</h5></div>";
-	
+	content.innerHTML = "<div class=" + "map-tooltip" + "><h4><strong>" + maptooltipbold + "</strong></h4><hr>" + "<h5>" + maptooltip + "</h5></div>";
+
 	// Initialize tooltips
 	var infowindow = new google.maps.InfoWindow({
-	 content: content
+		content: content
 	});
-	
+
 	var marker = new google.maps.Marker({
 		position: myLatlng,
 		map: map,
-		draggable:false,
+		draggable: false,
 		icon: image
 		// ,
 		// animation: google.maps.Animation.BOUNCE
-		
-	  });
+
+	});
 
 
-		google.maps.event.addListener(marker, 'click', function() {
-		  infowindow.open(map, marker);
-		});
+	google.maps.event.addListener(marker, 'click', function () {
+		infowindow.open(map, marker);
+	});
 
 }
