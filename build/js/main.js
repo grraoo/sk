6c9848a5966f4277b938e1e0ee72a3c55b158d9f
Grraoo <evil@grraoo.ru>
2017-08-25 17:13:37 +0800

    ready to show

--- build/js/main.js
+++ build/js/main.js
@@ -58,22 +58,7 @@ function init() {
 		zoom: 18,
 		
 		// The latitude and longitude to center the map (always required)
-		center: myLatlng,
-		
-		// How you would like to style the map. 
-		// This is where you would paste any style found on Snazzy Maps.
-		styles: [{featureType:"landscape",
-		stylers:[{saturation:-100},{lightness:55},{visibility:"on"}]},{featureType:"poi",
-		stylers:[{saturation:-100},{lightness:51},{visibility:"simplified"}]},{featureType:"road.highway",
-		stylers:[{saturation:-100},{visibility:"simplified"}]},{featureType:"road.arterial",
-		stylers:[{saturation:-100},{lightness:30},{visibility:"on"}]},{featureType:"road.local",
-		stylers:[{saturation:-100},{lightness:40},{visibility:"on"}]},{featureType:"transit",
-		stylers:[{saturation:-50},{visibility:"on"}]},{featureType:"administrative.province",
-		stylers:[{visibility:"off"}]/**/},{featureType:"administrative.locality",
-		stylers:[{visibility:"off"}]},{featureType:"administrative.neighborhood",
-		stylers:[{visibility:"on"}]/**/},{featureType:"water",elementType:"labels",
-		stylers:[{visibility:"on"},{lightness:-25},{saturation:-100}]},{featureType:"water",elementType:"geometry",
-		stylers:[{hue:"#ffff00"},{lightness:-25},{saturation:-97}]}]
+		center: myLatlng
 	};
 
 
